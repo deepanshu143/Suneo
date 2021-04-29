6fb02de55b8ea1a741f3fe99295e5b1628856ce0
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-04-29 18:28:27 +0530

    Update feds.py

--- LEGEND/modules/feds.py
+++ LEGEND/modules/feds.py
@@ -2393,10 +2393,35 @@ But then you have many groups, and you don't want this spammer to be in one of y
 You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
 
 *Commands:*\n
-Feds are now divided into 3 sections for your ease. 
-✪ /fedownerhelp*:* Provides help for fed creation and owner only commands
-✪ /fedadminhelp*:* Provides help for fed administration commands
-✪ /feduserhelp*:* Provides help for commands anyone can use
+👑 Fed Owner Only:
+ • /newfed <fed_name>: Creates a Federation, One allowed per user
+ • /renamefed <fed_id> <new_fed_name>: Renames the fed id to a new name
+ • /delfed <fed_id>: Delete a Federation, and any information related to it. Will not cancel blocked users
+ • /fpromote <user>: Assigns the user as a federation admin. Enables all commands for the user under Fed Admins
+ • /fdemote <user>: Drops the User from the admin Federation to a normal User
+ • /subfed <fed_id>: Subscribes to a given fed ID, bans from that subscribed fed will also happen in your fed
+ • /unsubfed <fed_id>: Unsubscribes to a given fed ID
+ • /setfedlog <fed_id>: Sets the group as a fed log report base for the federation
+ • /unsetfedlog <fed_id>: Removed the group as a fed log report base for the federation
+ • /fbroadcast <message>: Broadcasts a messages to all groups that have joined your fed
+ • /fedsubs: Shows the feds your group is subscribed to (broken rn)
+
+🔱 Fed Admins:
+ • /fban <user> <reason>: Fed bans a user
+ • /unfban <user> <reason>: Removes a user from a fed ban
+ • /fedinfo <fed_id>: Information about the specified Federation
+ • /joinfed <fed_id>: Join the current chat to the Federation. Only chat owners can do this. Every chat can only be in one Federation
+ • /leavefed <fed_id>: Leave the Federation given. Only chat owners can do this
+ • /setfrules <rules>: Arrange Federation rules
+ • /fedadmins: Show Federation admin
+ • /fbanlist: Displays all users who are victimized at the Federation at this time
+ • /fedchats: Get all the chats that are connected in the Federation
+ • /chatfed : See the Federation in the current chat
+
+🎩 For users:
+ • /fbanstat: Shows if you/or the user you are replying to or their username is fbanned somewhere or not
+ • /fednotif <on/off>: Federation settings not in PM when there are users who are fbaned/unfbanned
+ • /frules: See Federation regulations
 
 """
 
