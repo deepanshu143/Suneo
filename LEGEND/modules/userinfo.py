3a939e01c20ccf904c021c26392cd4c018460f38
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-05-04 20:15:32 +0530

    Update userinfo.py
    Link changed

--- LEGEND/modules/userinfo.py
+++ LEGEND/modules/userinfo.py
@@ -309,7 +309,7 @@ def info(update: Update, context: CallbackContext):
 
     if disaster_level_present:
 
-        text += ' [<a href="https://t.me/SuzuyaUpdates/55">?</a>]'.format(bot.username)
+        text += ' [<a href="https://t.me/SuneoSupportchannel/11">?</a>]'.format(bot.username)
 
     try:
         user_member = chat.get_member(user.id)
