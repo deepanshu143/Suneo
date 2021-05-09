0d20b711e9fce8a4f20a94c767f01487d3146ae4
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-05-09 18:58:02 +0530

    Update youtube.py

--- LEGEND/modules/youtube.py
+++ LEGEND/modules/youtube.py
@@ -74,6 +74,6 @@ async def inline_id_handler(event: events.InlineQuery.Event):
 
 __mod_name__="YouTube"
 __help__ = """
- - `@grand50_bot yt <search your query> ;8`
+ - `@SuneoManagerbot yt <search your query> ;8`
     Use ; this as result stopper
 """
