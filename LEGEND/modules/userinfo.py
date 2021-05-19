7d9884537d81934976ac254a869526795c3d94c6
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-05-19 08:39:23 +0530

    Update userinfo.py

--- LEGEND/modules/userinfo.py
+++ LEGEND/modules/userinfo.py
@@ -549,7 +549,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ Come and see [HP System explained](https://t.me/SuneoAndLucarioUpdates/11)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
