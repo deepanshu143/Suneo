82b12caab40046b9030df0f1f53fba866a1326b7
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-05-21 14:26:21 +0530

    Update disasters.py

--- LEGEND/modules/disasters.py
+++ LEGEND/modules/disasters.py
@@ -667,6 +667,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
+__mod_name__ = "Disasters ☠️"
 __handlers__ = [
     DEV_HANDLER,
     SUDO_HANDLER,
