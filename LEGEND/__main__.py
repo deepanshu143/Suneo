dbbcbb657c47899dfd5d7cbf95ebe15e3ebe27ab
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-05-30 15:08:32 +0530

    Update __main__.py

--- LEGEND/__main__.py
+++ LEGEND/__main__.py
@@ -65,10 +65,8 @@ from LEGEND.modules.helper_funcs.chat_status import is_user_admin
 from LEGEND.modules.helper_funcs.misc import paginate_modules
 from LEGEND.modules.helper_funcs.readable_time import get_readable_time
 
-legendx = event.sender.first_name
-
 PM_START_TEXT = """
-Hello {legendx} , I'm [Suneo](https://telegra.ph/file/4233b9a65aadbdf2cd682.jpg)
+Hello , I'm [Suneo](https://telegra.ph/file/4233b9a65aadbdf2cd682.jpg)
 
 I am an 𝐴𝑛𝑖𝑚𝑒 Themed Group Managing Bot Made by [This Person](https://t.me/MTY_IZ) and I will help in managing your group.You can find the list of available commands with /help
 
