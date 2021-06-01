e6c6d2f8187d4104ad8fbae6f500b62bd02f3546
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-06-01 17:29:17 +0530

    Update rules.py

--- LEGEND/modules/rules.py
+++ LEGEND/modules/rules.py
@@ -108,7 +108,6 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
  • `/rules`*:* get the rules for this chat.
-
 *Admins only:*
  • `/setrules <your rules here>`*:* set the rules for this chat.
  • `/clearrules`*:* clear the rules for this chat.
