ab639f7f5ca872e5eab4e00dafd3c7be6a2562e3
deepanshu143 <79148176+deepanshu143@users.noreply.github.com>
2021-07-06 13:35:58 +0530

    Start Msg Changed

--- LEGEND/__main__.py
+++ LEGEND/__main__.py
@@ -68,7 +68,7 @@ from LEGEND.modules.helper_funcs.readable_time import get_readable_time
 PM_START_TEXT = """
 Hello , I'm [Suneo](https://telegra.ph/file/4233b9a65aadbdf2cd682.jpg)
 
-I am an 𝐴𝑛𝑖𝑚𝑒 Themed Group Managing Bot Made by [This Person](https://t.me/MTY_IZ) and I will help in managing your group.You can find the list of available commands with /help. Inspired from SaitamaRobot ❤️. specialize in managing anime and similar themed groups.
+I am an 𝐴𝑛𝑖𝑚𝑒 Themed Group Managing Bot Made by [This Person](https://t.me/MTY_IZ) and I will help in managing your group.You can find the list of available commands by clicking on Commands and help. Inspired from SaitamaRobot ❤️. specialize in managing anime and similar themed groups.
 
 ✪ Don't forget to join my [Updates Channel](https://t.me/SuneoAndLucarioUpdates) to get new updates about me ✪ 
 """
@@ -76,7 +76,7 @@ I am an 𝐴𝑛𝑖𝑚𝑒 Themed Group Managing Bot Made by [This Person](htt
 buttons = [
     [
         InlineKeyboardButton(
-            text="➕️ ADD Suneo TO YOUR GROUP ➕️",url="t.me/SuneoManagerbot?startgroup=true"),
+            text="➕️ ADD SUNEO TO YOUR GROUP ➕️",url="t.me/SuneoManagerbot?startgroup=true"),
     ],
     [
         InlineKeyboardButton(
